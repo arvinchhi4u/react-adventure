43a990a52b669323e3314ea11d7479c5b11ef528
Amit Vinchhi <arvinchhi4u@gmail.com>
2024-01-08 21:38:00 +0530

    demonstrate component composition

--- App.js
+++ App.js
@@ -1,8 +1,20 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
+const Title = () => {
+  return <h1>This is title</h1>;
+};
+
+// Component Composition:
+// a component referred in output of another component
+// here, <Title /> is referred in <Heading />
 const Heading = () => {
-  return <h1>Hello World from React Component</h1>;
+  return (
+    <div id="container">
+      <Title />
+      <h2>Hello World from React Composite Component!</h2>
+    </div>
+  );
 };
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
