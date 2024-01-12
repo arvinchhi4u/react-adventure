d80770d217b0f141ef2b894037b0194adbadb5ce
Amit Vinchhi <arvinchhi4u@gmail.com>
2024-01-12 18:16:39 +0530

    start restaurant app (create components dynamically)

--- App.js
+++ App.js
@@ -1,22 +1,673 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import LogoImage from "./assets/images/fast-food-logo.png";
 
-const Title = () => {
-  return <h1>This is title</h1>;
+/**
+ * Header
+ *  - Logo
+ *  - Nav Links
+ * Body
+ *  - Search
+ *  - Restaurant Container
+ *    - Restaurant Card
+ *      - Image
+ *      - Name of Restaurant, Star Rating, Cuisine, Delivery Time
+ * Footer
+ *  - Copyright
+ *  - Links
+ *  - Address
+ *  - Contact
+ */
+
+const Header = () => {
+  return (
+    <div className="header">
+      <div className="logo-container">
+        <img className="logo" src={LogoImage} alt="Company Logo" />
+      </div>
+      <div className="nav-items">
+        <ul>
+          <li>Home</li>
+          <li>Cart</li>
+          <li>About Us</li>
+          <li>Contct Us</li>
+        </ul>
+      </div>
+    </div>
+  );
 };
 
-// Component Composition:
-// a component referred in output of another component
-// here, <Title /> is referred in <Heading />
-const Heading = () => {
+const resList = [
+  {
+    info: {
+      id: "183054",
+      name: "Utsav Restaurant",
+      cloudinaryImageId: "d0ykfvwcxde3tqef66o4",
+      locality: "ST Road",
+      areaName: "ST Bus Stand",
+      costForTwo: "₹300 for two",
+      cuisines: ["Fast Food", "North Indian", "Beverages"],
+      avgRating: 4.3,
+      veg: true,
+      parentId: "15843",
+      avgRatingString: "4.3",
+      totalRatingsString: "1K+",
+      sla: {
+        deliveryTime: 25,
+        lastMileTravel: 0.2,
+        serviceability: "SERVICEABLE",
+        slaString: "20-25 mins",
+        lastMileTravelString: "0.2 km",
+        iconType: "ICON_TYPE_EMPTY",
+      },
+      availability: {
+        nextCloseTime: "2024-01-12 23:00:00",
+        opened: true,
+      },
+      badges: {
+        imageBadges: [
+          {
+            imageId: "v1695133679/badges/Pure_Veg111.png",
+            description: "pureveg",
+          },
+        ],
+      },
+      isOpen: true,
+      type: "F",
+      badgesV2: {
+        entityBadges: {
+          imageBased: {
+            badgeObject: [
+              {
+                attributes: {
+                  description: "pureveg",
+                  imageId: "v1695133679/badges/Pure_Veg111.png",
+                },
+              },
+            ],
+          },
+          textBased: {},
+          textExtendedBadges: {},
+        },
+      },
+      aggregatedDiscountInfoV3: {
+        header: "₹120 OFF",
+        subHeader: "ABOVE ₹199",
+        discountTag: "FLAT DEAL",
+      },
+      differentiatedUi: {
+        displayType: "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+        differentiatedUiMediaDetails: {
+          mediaType: "ADS_MEDIA_ENUM_IMAGE",
+          lottie: {},
+          video: {},
+        },
+      },
+      reviewsSummary: {},
+      displayType: "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+      restaurantOfferPresentationInfo: {},
+    },
+    analytics: {},
+    cta: {
+      link: "https://www.swiggy.com/restaurants/utsav-restaurant-st-road-st-bus-stand-junagadh-183054",
+      type: "WEBLINK",
+    },
+  },
+  {
+    info: {
+      id: "182841",
+      name: "Utsav Restaurant & Fast Food",
+      cloudinaryImageId: "vddfbdfzqbslfd7dn29w",
+      locality: "ST Road",
+      areaName: "ST Bus Stand",
+      costForTwo: "₹240 for two",
+      cuisines: ["Fast Food", "North Indian", "Beverages"],
+      avgRating: 4.4,
+      veg: true,
+      parentId: "361689",
+      avgRatingString: "4.4",
+      totalRatingsString: "1K+",
+      sla: {
+        deliveryTime: 24,
+        lastMileTravel: 0.2,
+        serviceability: "SERVICEABLE",
+        slaString: "20-25 mins",
+        lastMileTravelString: "0.2 km",
+        iconType: "ICON_TYPE_EMPTY",
+      },
+      availability: {
+        nextCloseTime: "2024-01-12 23:59:00",
+        opened: true,
+      },
+      badges: {
+        imageBadges: [
+          {
+            imageId: "v1690360529/Ratnesh_Badges/Only_on_swiggy_badge_4x.png",
+            description: "OnlyOnSwiggy",
+          },
+          {
+            imageId: "v1695133679/badges/Pure_Veg111.png",
+            description: "pureveg",
+          },
+        ],
+      },
+      isOpen: true,
+      type: "F",
+      badgesV2: {
+        entityBadges: {
+          imageBased: {
+            badgeObject: [
+              {
+                attributes: {
+                  description: "OnlyOnSwiggy",
+                  imageId:
+                    "v1690360529/Ratnesh_Badges/Only_on_swiggy_badge_4x.png",
+                },
+              },
+              {
+                attributes: {
+                  description: "pureveg",
+                  imageId: "v1695133679/badges/Pure_Veg111.png",
+                },
+              },
+            ],
+          },
+          textBased: {},
+          textExtendedBadges: {},
+        },
+      },
+      aggregatedDiscountInfoV3: {
+        header: "₹120 OFF",
+        subHeader: "ABOVE ₹199",
+        discountTag: "FLAT DEAL",
+      },
+      differentiatedUi: {
+        displayType: "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+        differentiatedUiMediaDetails: {
+          mediaType: "ADS_MEDIA_ENUM_IMAGE",
+          lottie: {},
+          video: {},
+        },
+      },
+      reviewsSummary: {},
+      displayType: "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+      restaurantOfferPresentationInfo: {},
+    },
+    analytics: {},
+    cta: {
+      link: "https://www.swiggy.com/restaurants/utsav-restaurant-and-fast-food-st-road-st-bus-stand-junagadh-182841",
+      type: "WEBLINK",
+    },
+  },
+  {
+    info: {
+      id: "182795",
+      name: "Ashok Baker's and Cake Shop",
+      cloudinaryImageId: "asvgvo3mchgka3rfrf4j",
+      locality: "College Road",
+      areaName: "Kadiyavad",
+      costForTwo: "₹500 for two",
+      cuisines: ["Bakery"],
+      avgRating: 4.4,
+      veg: true,
+      parentId: "36917",
+      avgRatingString: "4.4",
+      totalRatingsString: "500+",
+      sla: {
+        deliveryTime: 22,
+        lastMileTravel: 1.8,
+        serviceability: "SERVICEABLE",
+        slaString: "20-25 mins",
+        lastMileTravelString: "1.8 km",
+        iconType: "ICON_TYPE_EMPTY",
+      },
+      availability: {
+        nextCloseTime: "2024-01-12 21:45:00",
+        opened: true,
+      },
+      badges: {},
+      isOpen: true,
+      type: "F",
+      badgesV2: {
+        entityBadges: {
+          imageBased: {},
+          textBased: {},
+          textExtendedBadges: {},
+        },
+      },
+      aggregatedDiscountInfoV3: {
+        header: "₹120 OFF",
+        subHeader: "ABOVE ₹199",
+        discountTag: "FLAT DEAL",
+      },
+      differentiatedUi: {
+        displayType: "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+        differentiatedUiMediaDetails: {
+          mediaType: "ADS_MEDIA_ENUM_IMAGE",
+          lottie: {},
+          video: {},
+        },
+      },
+      reviewsSummary: {},
+      displayType: "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+      restaurantOfferPresentationInfo: {},
+    },
+    analytics: {},
+    cta: {
+      link: "https://www.swiggy.com/restaurants/ashok-bakers-and-cake-shop-college-road-kadiyavad-junagadh-182795",
+      type: "WEBLINK",
+    },
+  },
+  {
+    info: {
+      id: "750890",
+      name: "Samosazz",
+      cloudinaryImageId: "cfe95f51da4c6a9312ff0306c033f736",
+      locality: "Gunatit Plaza",
+      areaName: "Moti Baug",
+      costForTwo: "₹200 for two",
+      cuisines: ["Snacks", "Chinese", "Beverages", "Fast Food"],
+      avgRating: 4.1,
+      veg: true,
+      parentId: "264257",
+      avgRatingString: "4.1",
+      totalRatingsString: "20+",
+      sla: {
+        deliveryTime: 31,
+        lastMileTravel: 2.2,
+        serviceability: "SERVICEABLE",
+        slaString: "30-35 mins",
+        lastMileTravelString: "2.2 km",
+        iconType: "ICON_TYPE_EMPTY",
+      },
+      availability: {
+        nextCloseTime: "2024-01-12 23:00:00",
+        opened: true,
+      },
+      badges: {
+        imageBadges: [
+          {
+            imageId: "v1695133679/badges/Pure_Veg111.png",
+            description: "pureveg",
+          },
+        ],
+      },
+      isOpen: true,
+      type: "F",
+      badgesV2: {
+        entityBadges: {
+          imageBased: {
+            badgeObject: [
+              {
+                attributes: {
+                  description: "pureveg",
+                  imageId: "v1695133679/badges/Pure_Veg111.png",
+                },
+              },
+            ],
+          },
+          textBased: {},
+          textExtendedBadges: {},
+        },
+      },
+      aggregatedDiscountInfoV3: {
+        header: "₹120 OFF",
+        subHeader: "ABOVE ₹199",
+        discountTag: "FLAT DEAL",
+      },
+      differentiatedUi: {
+        displayType: "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+        differentiatedUiMediaDetails: {
+          mediaType: "ADS_MEDIA_ENUM_IMAGE",
+          lottie: {},
+          video: {},
+        },
+      },
+      reviewsSummary: {},
+      displayType: "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+      isNewlyOnboarded: true,
+      restaurantOfferPresentationInfo: {},
+    },
+    analytics: {},
+    cta: {
+      link: "https://www.swiggy.com/restaurants/samosazz-gunatit-plaza-moti-baug-junagadh-750890",
+      type: "WEBLINK",
+    },
+  },
+  {
+    info: {
+      id: "713130",
+      name: "Vanzari Friends And Family Food",
+      cloudinaryImageId: "cff246ec70b613fa50b2c74c1d6e63c6",
+      locality: "Junagadh",
+      areaName: "Vanzari Chowk",
+      costForTwo: "₹170 for two",
+      cuisines: ["Gujarati", "Street Food"],
+      avgRating: 4.6,
+      veg: true,
+      parentId: "425754",
+      avgRatingString: "4.6",
+      totalRatingsString: "20+",
+      sla: {
+        deliveryTime: 23,
+        lastMileTravel: 1,
+        serviceability: "SERVICEABLE",
+        slaString: "20-25 mins",
+        lastMileTravelString: "1.0 km",
+        iconType: "ICON_TYPE_EMPTY",
+      },
+      availability: {
+        nextCloseTime: "2024-01-12 20:00:00",
+        opened: true,
+      },
+      badges: {
+        imageBadges: [
+          {
+            imageId: "v1695133679/badges/Pure_Veg111.png",
+            description: "pureveg",
+          },
+        ],
+      },
+      isOpen: true,
+      type: "F",
+      badgesV2: {
+        entityBadges: {
+          imageBased: {
+            badgeObject: [
+              {
+                attributes: {
+                  description: "pureveg",
+                  imageId: "v1695133679/badges/Pure_Veg111.png",
+                },
+              },
+            ],
+          },
+          textBased: {},
+          textExtendedBadges: {},
+        },
+      },
+      aggregatedDiscountInfoV3: {
+        header: "₹120 OFF",
+        subHeader: "ABOVE ₹199",
+        discountTag: "FLAT DEAL",
+      },
+      differentiatedUi: {
+        displayType: "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+        differentiatedUiMediaDetails: {
+          mediaType: "ADS_MEDIA_ENUM_IMAGE",
+          lottie: {},
+          video: {},
+        },
+      },
+      reviewsSummary: {},
+      displayType: "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+      restaurantOfferPresentationInfo: {},
+    },
+    analytics: {},
+    cta: {
+      link: "https://www.swiggy.com/restaurants/vanzari-friends-and-family-food-vanzari-chowk-junagadh-713130",
+      type: "WEBLINK",
+    },
+  },
+  {
+    info: {
+      id: "714497",
+      name: "Murlidhar Dairy Farm",
+      cloudinaryImageId: "616e0e302b6265bf46f0bec0a80be1d4",
+      locality: "M G Road",
+      areaName: "MG Road",
+      costForTwo: "₹200 for two",
+      cuisines: ["Sweets", "Gujarati"],
+      avgRating: 4.3,
+      veg: true,
+      parentId: "426451",
+      avgRatingString: "4.3",
+      totalRatingsString: "10+",
+      sla: {
+        deliveryTime: 20,
+        lastMileTravel: 1,
+        serviceability: "SERVICEABLE",
+        slaString: "20-25 mins",
+        lastMileTravelString: "1.0 km",
+        iconType: "ICON_TYPE_EMPTY",
+      },
+      availability: {
+        nextCloseTime: "2024-01-12 21:00:00",
+        opened: true,
+      },
+      badges: {
+        imageBadges: [
+          {
+            imageId: "v1695133679/badges/Pure_Veg111.png",
+            description: "pureveg",
+          },
+        ],
+      },
+      isOpen: true,
+      type: "F",
+      badgesV2: {
+        entityBadges: {
+          imageBased: {
+            badgeObject: [
+              {
+                attributes: {
+                  description: "pureveg",
+                  imageId: "v1695133679/badges/Pure_Veg111.png",
+                },
+              },
+            ],
+          },
+          textBased: {},
+          textExtendedBadges: {},
+        },
+      },
+      aggregatedDiscountInfoV3: {
+        header: "₹120 OFF",
+        subHeader: "ABOVE ₹199",
+        discountTag: "FLAT DEAL",
+      },
+      differentiatedUi: {
+        displayType: "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+        differentiatedUiMediaDetails: {
+          mediaType: "ADS_MEDIA_ENUM_IMAGE",
+          lottie: {},
+          video: {},
+        },
+      },
+      reviewsSummary: {},
+      displayType: "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+      restaurantOfferPresentationInfo: {},
+    },
+    analytics: {},
+    cta: {
+      link: "https://www.swiggy.com/restaurants/murlidhar-dairy-farm-m-g-road-mg-road-junagadh-714497",
+      type: "WEBLINK",
+    },
+  },
+  {
+    info: {
+      id: "183202",
+      name: "Chamunda Lassi Shop",
+      cloudinaryImageId: "fnxhmsfhysocjxtkenam",
+      locality: "ST Road",
+      areaName: "ST Bus Stand",
+      costForTwo: "₹300 for two",
+      cuisines: ["Beverages"],
+      avgRating: 4.6,
+      veg: true,
+      parentId: "57648",
+      avgRatingString: "4.6",
+      totalRatingsString: "1K+",
+      sla: {
+        deliveryTime: 15,
+        serviceability: "SERVICEABLE",
+        slaString: "10-15 mins",
+        iconType: "ICON_TYPE_EMPTY",
+      },
+      availability: {
+        nextCloseTime: "2024-01-12 23:30:00",
+        opened: true,
+      },
+      badges: {},
+      isOpen: true,
+      type: "F",
+      badgesV2: {
+        entityBadges: {
+          imageBased: {},
+          textBased: {},
+          textExtendedBadges: {},
+        },
+      },
+      aggregatedDiscountInfoV3: {
+        header: "₹120 OFF",
+        subHeader: "ABOVE ₹199",
+        discountTag: "FLAT DEAL",
+      },
+      differentiatedUi: {
+        displayType: "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+        differentiatedUiMediaDetails: {
+          mediaType: "ADS_MEDIA_ENUM_IMAGE",
+          lottie: {},
+          video: {},
+        },
+      },
+      reviewsSummary: {},
+      displayType: "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+      restaurantOfferPresentationInfo: {},
+    },
+    analytics: {},
+    cta: {
+      link: "https://www.swiggy.com/restaurants/chamunda-lassi-shop-st-road-st-bus-stand-junagadh-183202",
+      type: "WEBLINK",
+    },
+  },
+  {
+    info: {
+      id: "679691",
+      name: "Speed Food Parcel",
+      cloudinaryImageId: "9e8796c3065fe1ea0f2d80664f8388f4",
+      locality: "Jayshri cinemajayshri road",
+      areaName: "Jayshree Cinema",
+      costForTwo: "₹250 for two",
+      cuisines: ["North Indian", "Chinese", "South Indian"],
+      avgRating: 4.4,
+      veg: true,
+      parentId: "191913",
+      avgRatingString: "4.4",
+      totalRatingsString: "100+",
+      sla: {
+        deliveryTime: 21,
+        lastMileTravel: 1.3,
+        serviceability: "SERVICEABLE",
+        slaString: "20-25 mins",
+        lastMileTravelString: "1.3 km",
+        iconType: "ICON_TYPE_EMPTY",
+      },
+      availability: {
+        nextCloseTime: "2024-01-12 22:30:00",
+        opened: true,
+      },
+      badges: {
+        imageBadges: [
+          {
+            imageId: "v1695133679/badges/Pure_Veg111.png",
+            description: "pureveg",
+          },
+        ],
+      },
+      isOpen: true,
+      type: "F",
+      badgesV2: {
+        entityBadges: {
+          imageBased: {
+            badgeObject: [
+              {
+                attributes: {
+                  description: "pureveg",
+                  imageId: "v1695133679/badges/Pure_Veg111.png",
+                },
+              },
+            ],
+          },
+          textBased: {},
+          textExtendedBadges: {},
+        },
+      },
+      aggregatedDiscountInfoV3: {
+        header: "₹120 OFF",
+        subHeader: "ABOVE ₹199",
+        discountTag: "FLAT DEAL",
+      },
+      differentiatedUi: {
+        displayType: "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+        differentiatedUiMediaDetails: {
+          mediaType: "ADS_MEDIA_ENUM_IMAGE",
+          lottie: {},
+          video: {},
+        },
+      },
+      reviewsSummary: {},
+      displayType: "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+      restaurantOfferPresentationInfo: {},
+    },
+    analytics: {},
+    cta: {
+      link: "https://www.swiggy.com/restaurants/speed-food-parcel-jayshri-cinemajayshri-road-jayshree-cinema-junagadh-679691",
+      type: "WEBLINK",
+    },
+  },
+];
+
+const RestaurantCard = (props) => {
+  console.log(props);
+  const { resData } = props;
+  console.log("Hi " + resData.info.name);
+  const { cloudinaryImageId, name, avgRating, cuisines, costForTwo } =
+    resData?.info; // data destructuring
+  const { deliveryTime } = resData?.info.sla;
+
   return (
-    <div id="container">
-      <Title />
-      <h2>Hello World from React Composite Component!</h2>
+    <div className="res-card" style={{ backgroundColor: "#f0f0f0" }}>
+      <div className="res-logo-container">
+        <img
+          src={
+            "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/" +
+            cloudinaryImageId
+          }
+          alt="Restaurant Logo"
+          className="res-logo"
+        />
+      </div>
+      <h3>{name}</h3>
+      <h4>{cuisines.join(", ")}</h4>
+      <h4>{avgRating} stars</h4>
+      <h4>{costForTwo}</h4>
+      <h4>{deliveryTime} minutes</h4>
+    </div>
+  );
+};
+
+const Body = () => {
+  return (
+    <div className="body">
+      <div className="search">Search</div>
+      <div className="res-container">
+        {resList.map((restaurant) => (
+          <RestaurantCard key={restaurant.info.name} resData={restaurant} />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+const AppLayout = () => {
+  return (
+    <div className="app">
+      <Header />
+      <Body />
     </div>
   );
 };
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-
-root.render(<Heading />);
+root.render(<AppLayout />);
