bdb03e84478a0723dd3ce2ea43643a8a8632b298
Amit Vinchhi <arvinchhi4u@gmail.com>
2024-01-18 18:42:10 +0530

    apply styles to shimmer ui using tailwind

--- src/components/Shimmer.js
+++ src/components/Shimmer.js
@@ -1,21 +1,21 @@
 const Shimmer = () => {
   return (
-    <div className="shimmer-container">
-      <div className="shimmer-card"></div>
-      <div className="shimmer-card"></div>
-      <div className="shimmer-card"></div>
-      <div className="shimmer-card"></div>
-      <div className="shimmer-card"></div>
-      <div className="shimmer-card"></div>
-      <div className="shimmer-card"></div>
-      <div className="shimmer-card"></div>
-      <div className="shimmer-card"></div>
-      <div className="shimmer-card"></div>
-      <div className="shimmer-card"></div>
-      <div className="shimmer-card"></div>
-      <div className="shimmer-card"></div>
-      <div className="shimmer-card"></div>
-      <div className="shimmer-card"></div>
+    <div className="shimmer-container flex flex-wrap justify-center">
+      <div className="shimmer-card m-4 p-4 w-[250px] h-[400px] rounded-lg bg-gray-100"></div>
+      <div className="shimmer-card m-4 p-4 w-[250px] h-[400px] rounded-lg bg-gray-100"></div>
+      <div className="shimmer-card m-4 p-4 w-[250px] h-[400px] rounded-lg bg-gray-100"></div>
+      <div className="shimmer-card m-4 p-4 w-[250px] h-[400px] rounded-lg bg-gray-100"></div>
+      <div className="shimmer-card m-4 p-4 w-[250px] h-[400px] rounded-lg bg-gray-100"></div>
+      <div className="shimmer-card m-4 p-4 w-[250px] h-[400px] rounded-lg bg-gray-100"></div>
+      <div className="shimmer-card m-4 p-4 w-[250px] h-[400px] rounded-lg bg-gray-100"></div>
+      <div className="shimmer-card m-4 p-4 w-[250px] h-[400px] rounded-lg bg-gray-100"></div>
+      <div className="shimmer-card m-4 p-4 w-[250px] h-[400px] rounded-lg bg-gray-100"></div>
+      <div className="shimmer-card m-4 p-4 w-[250px] h-[400px] rounded-lg bg-gray-100"></div>
+      <div className="shimmer-card m-4 p-4 w-[250px] h-[400px] rounded-lg bg-gray-100"></div>
+      <div className="shimmer-card m-4 p-4 w-[250px] h-[400px] rounded-lg bg-gray-100"></div>
+      <div className="shimmer-card m-4 p-4 w-[250px] h-[400px] rounded-lg bg-gray-100"></div>
+      <div className="shimmer-card m-4 p-4 w-[250px] h-[400px] rounded-lg bg-gray-100"></div>
+      <div className="shimmer-card m-4 p-4 w-[250px] h-[400px] rounded-lg bg-gray-100"></div>
     </div>
   );
 };
